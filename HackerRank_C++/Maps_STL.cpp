90e2fac6e69651b4b5f0f28445980b8c00139f1b
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-01-25 09:19:41 +0530

    Create Maps_STL.cpp

---
+++ HackerRank_C++/Maps_STL.cpp
@@ -0,0 +1,46 @@
+
+// https://www.hackerrank.com/challenges/cpp-maps/problem?isFullScreen=false
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+    int q, type; 
+    
+    cin >> q; 
+    
+    map<string,int> clas ; 
+    string name;
+    
+    for (int i=0, mark; i<q; ++i)
+    {
+        cin >> type >> name;
+        
+        if (type == 1)
+        {
+            cin >> mark;
+            clas[name] += mark;
+        }
+        
+        else if (type == 2)
+        {
+            clas.erase(name);
+        }
+        
+        else
+        {
+            cout << clas[name] << "\n";
+        }
+        
+    }
+    return 0;
+}
