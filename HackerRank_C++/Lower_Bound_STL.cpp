2503a335760b7e08621931d9cae4ccb8fc0bf1eb
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-01-27 12:13:20 +0530

    Create Lower_Bound_STL.cpp

---
+++ HackerRank_C++/Lower_Bound_STL.cpp
@@ -0,0 +1,55 @@
+
+// https://www.hackerrank.com/challenges/cpp-lower-bound/problem
+
+
+
+#include <bits/stdc++.h>        // universal set of all the header files in c++
+
+using namespace std ;
+
+int main ()
+{
+
+    int sizee , nquery ;
+
+    // input of size of the array
+    cin >> sizee ;
+
+    int arr[sizee] ;
+
+    for (int i = 0 ; i < sizee ; i++ )
+    {
+        cin >> arr[i] ;
+    }
+    
+    // int *ptr = arr[sizee] ;
+
+
+    // input of no. of queries 
+    cin >> nquery ; 
+
+    int quer[nquery] ;
+
+    for (int i = 0 ; i < nquery ; i++ )
+    {
+        cin >> quer[i] ;
+    }
+
+    for (int i = 0 ; i < nquery ; i++ )
+    {
+        auto itr = lower_bound(arr , arr+sizee , quer[i] ) - arr ;
+        auto itr1 = upper_bound(arr , arr+sizee , quer[i] ) - arr ;
+
+        if ( itr == itr1 )
+        {
+            cout << "No " << (itr+1) <<endl;
+        }
+
+        else 
+        {
+            cout << "Yes " << (itr +1) << endl;
+        }
+    }
+    
+    return 0 ;
+}
