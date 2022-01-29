eaf52ddb78cd7d1c53ea3aac2dcbfa4644b4656e
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-01-29 19:55:43 +0530

    Create Find_the_Point.cpp

---
+++ HackerRank_Mathematics/Find_the_Point.cpp
@@ -0,0 +1,24 @@
+
+// https://www.hackerrank.com/challenges/find-point/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int px,py,qx,qy,rx,ry,test;
+
+    cin >> test ;
+
+    for(int i = 0; i < test ; i++)
+    {
+        cin >> px >> py >> qx >> qy ;
+
+        rx = qx + (qx-px) ;
+        ry = qy + (qy-py) ;
+
+        cout << rx << " " << ry << endl ;
+    }
+    
+    return 0;
+}
