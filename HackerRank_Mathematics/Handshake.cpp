c1aa5bd51993eb6d9910c8a65377f30e0080b275
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-01-31 10:13:46 +0530

    Create Handshake.cpp

---
+++ HackerRank_Mathematics/Handshake.cpp
@@ -0,0 +1,29 @@
+
+// https://www.hackerrank.com/challenges/handshake/problem?isFullScreen=false
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int test , n ,hand;
+
+    cin >> test ;
+    int arr[test] ;
+
+    for(int i=0 ; i < test ; i++)
+    {
+        cin >> n ;
+        hand = ((n*(n-1))/2 );
+
+        arr[i] = hand ; 
+    }
+
+    for(int i=0 ; i < test ; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    
+    return 0;
+}
