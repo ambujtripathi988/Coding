0495118c87df52dbff45a2f661d525bcd1e86756
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-01 12:44:28 +0530

    Create Minimum_Height_Triangle.cpp

---
+++ HackerRank_Mathematics/Minimum_Height_Triangle.cpp
@@ -0,0 +1,23 @@
+
+// https://www.hackerrank.com/challenges/lowest-triangle/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    double a,b;
+    double h;
+
+    cin >> b >> a ;
+
+    h = (a*2)/b ;
+    
+    long long int num = ceil(h) ;
+    
+
+    cout << num ;
+
+    
+    return 0;
+}
