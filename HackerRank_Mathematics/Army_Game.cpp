6fba78b24085edfa30ecfd0f268c98976fe8d8d9
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-02 10:36:20 +0530

    Create Army_Game.cpp

---
+++ HackerRank_Mathematics/Army_Game.cpp
@@ -0,0 +1,18 @@
+
+// https://www.hackerrank.com/challenges/game-with-cells/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n,m ;
+
+    cin >> n >> m ;
+
+    int drop = ((n+1)/2)*((m+1)/2) ;
+
+    cout << drop ;
+
+    return 0;
+}
