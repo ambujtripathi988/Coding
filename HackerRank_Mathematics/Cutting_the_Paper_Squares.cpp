0bb72fade2b972d8001978639fb98e78f1e502fb
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-04 14:55:00 +0530

    Create Cutting_the_Paper_Squares.cpp

---
+++ HackerRank_Mathematics/Cutting_the_Paper_Squares.cpp
@@ -0,0 +1,18 @@
+
+// https://www.hackerrank.com/challenges/p1-paper-cutting/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    long int n , m , cuts ;
+
+    cin >> n >> m ;
+    
+    cuts = (n*m) - 1 ;
+
+    cout << cuts << endl ;
+
+    return 0;
+}
