a1ed764f594dba49888820ac9bd070b25eb45f28
Ambuj Tripathi <92564160+ambujtripathi988@users.noreply.github.com>
2022-02-08 12:36:35 +0530

    Update Reverse_Game.cpp

--- HackerRank_Mathematics/Reverse_Game.cpp
+++ HackerRank_Mathematics/Reverse_Game.cpp
@@ -3,7 +3,7 @@
 
 #include <bits/stdc++.h>
 
-using namespace std;
+using namespace std ;
 
 int main()
 {
@@ -21,7 +21,7 @@ int main()
         
         for(int i=0;i<n;i++)
         {
-            a[i] = i;
+            a[i] = i ;
         }
         int i,j,x;
         
