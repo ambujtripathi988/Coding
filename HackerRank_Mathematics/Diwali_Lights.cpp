76c9b6bc92be8ee16c8840691ea72f030965e4de
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-09 12:57:02 +0530

    Create Diwali_Lights.cpp

---
+++ HackerRank_Mathematics/Diwali_Lights.cpp
@@ -0,0 +1,33 @@
+
+// https://www.hackerrank.com/challenges/diwali-lights/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test  ;
+    int num ;
+    int patterns = 1 ;
+
+    cin >> test ;
+
+    for (int i = 0; i < test; ++i)
+    {
+        cin >> num ;
+        
+        for(int j =0; j < num ; j++)
+        {
+            patterns = patterns * 2 ;
+            
+            patterns = patterns % 100000 ;
+        }
+        
+        cout << (patterns-1) << endl ;
+        
+        patterns = 1 ;
+
+    }
+
+    return 0;
+}
