78c6e10e266d5b9520704ca4a052bdb19c64fbba
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-16 10:37:10 +0530

    Create Strange_Grid_Again.cpp

---
+++ HackerRank_Mathematics/Strange_Grid_Again.cpp
@@ -0,0 +1,35 @@
+
+// https://www.hackerrank.com/challenges/strange-grid/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long int row ;
+    int column , element ;
+
+    cin >> row >> column ;
+    
+    /*
+
+    if(row % 2 != 0)            // for odd rows
+    {
+        element = (row/2)*10 + ((2*column)-2) ;
+    }
+
+    else                        // for even rows
+    {
+        element = ((row/2)-1)*10 + ((2*column)-1) ;
+    }
+
+    cout << element ;
+    
+    */
+    
+    cout << (row-1)/2*10+2*(column-1)+(row-1)%2 ;
+
+
+    return 0;
+}
