eb07169c4d2532daaa9642a92df6e24e99570aef
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-18 10:42:07 +0530

    Create Halloween_Party.cpp

---
+++ HackerRank_Mathematics/Halloween_Party.cpp
@@ -0,0 +1,41 @@
+
+// https://www.hackerrank.com/challenges/halloween-party/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test ;
+
+    cin >> test ;
+
+    for(int i = 0 ; i<test ; i++)
+    {
+        long int cuts , hori_cuts , verti_cuts , pieces ;
+        cin >> cuts ;
+
+        if(cuts % 2 == 0 )
+        {
+            hori_cuts = cuts / 2 ;
+            verti_cuts = cuts / 2 ; 
+
+            pieces = hori_cuts * verti_cuts ;
+
+            cout << pieces << endl ;
+        }
+
+        else 
+        {
+            hori_cuts = cuts / 2 ;
+            verti_cuts = cuts / 2 ;
+            verti_cuts ++ ;
+
+            pieces = hori_cuts * verti_cuts ;
+            
+            cout << pieces << endl ;
+        }
+    }
+
+    return 0;
+}
