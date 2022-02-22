696cc423bff5845fa09e43128e980b11f932c14d
Ambuj Tripathi <92564160+ambujtripathi988@users.noreply.github.com>
2022-02-22 19:10:01 +0530

    Update Simple_Array_Sum.cpp

--- HackerRank_Problem_Solving/Simple_Array_Sum.cpp
+++ HackerRank_Problem_Solving/Simple_Array_Sum.cpp
@@ -8,14 +8,19 @@ using namespace std;
 int main()
 {
     int no_of_elements ;
+    
     cin >> no_of_elements ;
+    
     // int arr[no_of_elements] ;
+    
     int sum = 0;
     
     for(int i = 0 ; i < no_of_elements ; i++)
     {
         int element ;
+        
         cin >> element ;
+        
         sum = sum + element ;
     
     }
