7d7f18904b404cbf4c10f5cfb3e6dbef2084c918
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-23 16:39:24 +0530

    Create Compare_the_Triplets.cpp

---
+++ HackerRank_Problem_Solving/Compare_the_Triplets.cpp
@@ -0,0 +1,39 @@
+
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    
+    int a[3] , b[3] ;
+    int alex = 0 , bob = 0 ;
+
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        cin >> a[i] ;
+    }
+
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        cin >> b[i] ;
+    }
+
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        if(a[i] > b[i])
+        {
+            alex ++ ;
+        }
+
+        else if(b[i] > a[i])
+        {
+            bob ++ ;
+        }
+    }
+
+    cout << alex << " " << bob ;
+
+    return 0;
+}
