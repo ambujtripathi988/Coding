c2345fa169fa38e187cdba84ca6bb88bfde47d27
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-26 09:50:58 +0530

    Create Mini_Max_Sum.cpp

---
+++ HackerRank_Problem_Solving/Mini_Max_Sum.cpp
@@ -0,0 +1,36 @@
+
+// https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    vector<int> arr;
+    
+    long int max = 0 , min = 0 ;
+
+    for(int i = 0 ; i < 5 ; i++)
+    {
+        int element ;
+        cin >> element ;
+        
+        arr.push_back(element) ;
+    }
+
+    sort(arr.begin(), arr.end()) ;
+
+    for (int i = 5; i > 1; i--)
+    {
+        max = max + arr[i-1] ;
+    }
+
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        min = min + arr[i] ;
+    }
+
+    cout << min << " " << max ;
+
+    return 0;
+}
