4ba8f8f8fb025e3029aa961fd356a7a7f3fd57a8
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-02-28 00:06:29 +0530

    Create Birthday_Cake_Candles.cpp

---
+++ HackerRank_Problem_Solving/Birthday_Cake_Candles.cpp
@@ -0,0 +1,39 @@
+
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n ;
+
+    cin >> n ;
+
+    vector<int> arr ;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int element ;
+        cin >> element ;
+        arr.push_back(element) ;
+    }
+
+    sort(arr.begin(),arr.end()) ;
+
+    int max_num = arr[n-1] ;
+
+    int count = 0 ;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(max_num == arr[i])
+        {
+            count ++ ;
+        }
+    } 
+
+    cout << count ;
+
+    return 0;
+}
