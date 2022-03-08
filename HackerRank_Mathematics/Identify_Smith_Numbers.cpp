d4e8522c1ae73471523af594f002620d50048e0c
Ambuj Tripathi <92564160+ambujtripathi988@users.noreply.github.com>
2022-03-08 20:47:50 +0530

    Update Identify_Smith_Numbers.cpp

--- HackerRank_Mathematics/Identify_Smith_Numbers.cpp
+++ HackerRank_Mathematics/Identify_Smith_Numbers.cpp
@@ -2,7 +2,7 @@
 // https://www.hackerrank.com/challenges/identify-smith-numbers/problem?h_r=profile
 
 #include <bits/stdc++.h> 
-using namespace std;
+using namespace std ;
 
 void primeFactors(int n , vector<int> &primes , int &count)  
 {   
