afca082be898466c54589cbede2a52320504b3f5
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-03-11 20:36:26 +0530

    Create Angry_Professor.cpp

---
+++ HackerRank_Problem_Solving/Angry_Professor.cpp
@@ -0,0 +1,49 @@
+
+// https://www.hackerrank.com/challenges/angry-professor/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test , n , k , count = 0 ;
+    
+    cin >> test ;
+
+    for(int j = 0 ; j < test ; j++ )
+    {
+        
+        cin >> n >> k ;
+
+        int arr[n] ;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            cin >> arr[i] ;
+        }
+
+        for(int i=0 ; i < n ; i++)
+        {
+            if(arr[i] <= 0)
+            {
+                count ++ ;
+            }
+        }
+
+        if (count >= k)
+        {
+            cout << "NO" << endl ;
+        }
+
+        else 
+        {
+            cout << "YES" << endl ;
+        }
+        
+        count = 0 ;
+
+    } 
+
+    
+    return 0;
+}
