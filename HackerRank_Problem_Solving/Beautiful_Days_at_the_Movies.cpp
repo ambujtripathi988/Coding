770c2b5992d459c5d410ab09c60fa92daac2ee46
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-03-14 15:48:03 +0530

    Create Beautiful_Days_at_the_Movies.cpp

---
+++ HackerRank_Problem_Solving/Beautiful_Days_at_the_Movies.cpp
@@ -0,0 +1,38 @@
+
+// https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int i , j , k , rev = 0 , digit , count = 0 ;
+
+    cin >> i >> j >> k ;
+
+    for(int s = i ; s <= j ; s++)
+    {
+        int s1 = s ; 
+        do
+         {
+            digit = s1 % 10;
+            rev = (rev * 10) + digit;
+            s1 = s1 / 10;
+
+         } while (s1 != 0);
+
+        int x = abs(s-rev) ;
+
+        if(x%k == 0)
+        {
+            count ++ ;
+        }
+
+        rev = 0 ;
+    }
+
+    cout << count ;
+
+    
+    return 0;
+}
