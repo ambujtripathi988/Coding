758e290b9ecd72b64f9ccecf8e1445b523c77643
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-03-15 09:32:10 +0530

    Create Viral_Advertising.cpp

---
+++ HackerRank_Problem_Solving/Viral_Advertising.cpp
@@ -0,0 +1,27 @@
+
+// https://www.hackerrank.com/challenges/strange-advertising/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n ; 
+
+    int ad = 5 , total_like = 0 , like_instant ;
+
+    cin >> n ;
+
+    for(int i = 1 ; i <= n ; i++)
+    {
+        like_instant = ad / 2 ;
+
+        total_like += like_instant ; 
+
+        ad = (ad/2)*3 ;
+    }
+
+    cout << total_like ;
+    
+    return 0;
+}
