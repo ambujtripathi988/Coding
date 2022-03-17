0d2c2bd3644c51b80b25e83859f31fb7055e4064
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-03-17 22:27:04 +0530

    Create Arrays_DS.cpp

---
+++ HackerRank_Problem_Solving/Arrays_DS.cpp
@@ -0,0 +1,25 @@
+
+// https://www.hackerrank.com/challenges/arrays-ds/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n ;
+    cin >> n ;
+
+    int arr[n] ;
+
+    for (int i = 0; i < n ; i++)
+    {
+        cin >> arr[i] ;
+    }
+
+    for(int i = (n-1) ; i >= 0 ; i--)
+    {
+        cout << arr[i] << " " ;
+    }
+
+    return 0;
+}
