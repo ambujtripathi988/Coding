f3bfb5b860468d802e955a4797408bcc1ee810ff
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-03-18 17:46:40 +0530

    Create Counting_Valleys.cpp

---
+++ HackerRank_Problem_Solving/Counting_Valleys.cpp
@@ -0,0 +1,42 @@
+
+// https://www.hackerrank.com/challenges/counting-valleys/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n , sea_level = 0 , valley = 0 ; 
+
+    cin >> n ;
+
+    // char path[n] ;
+
+    char paths ;
+
+    for (int i = 0; i < n; i++)
+    {
+        // cin >> path[i] ;
+
+        cin >> paths ;
+
+        if(paths=='U')
+        {
+            if (++sea_level == 0)
+            {
+                valley ++ ;
+            }
+        }
+
+        else
+        {
+            sea_level -- ;
+        }
+
+    }
+
+    cout << valley << endl ;
+
+
+    return 0;
+}
