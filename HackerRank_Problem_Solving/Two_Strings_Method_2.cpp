f81735865e16fb501ef00f95fbd4104cf8cee658
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-04-02 09:39:08 +0530

    Create Two_Strings_Method_2.cpp

---
+++ HackerRank_Problem_Solving/Two_Strings_Method_2.cpp
@@ -0,0 +1,54 @@
+
+// https://www.hackerrank.com/challenges/two-strings/problem?h_r=profile
+
+// Method 2 :
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long int test , flag = 0 ;
+
+    cin >> test ;
+
+    string str1 , str2 ;
+
+    for(int s = 0 ; s < test ; s++)
+    {
+        flag = 0 ;
+
+        cin >> str1 >> str2 ;
+
+        set<char> st1 ;
+        
+        flag = 0 ;
+        
+        for(int i = 0 ; i < str1.length() ; i++)
+        {
+            st1.insert(str1[i]) ;
+        }
+        
+        for(int i = 0 ; i < str2.length() ; i++)
+        {
+            if( (st1.find(str2[i])) != st1.end() )
+            {
+                flag = 1 ;
+                break ;
+            }
+        }
+        
+        if(flag == 1 )
+        {
+            cout << "YES" << endl ;
+        }
+
+        else
+        {
+            cout << "NO" << endl ; 
+        }
+        
+    }
+
+    return 0;
+}
