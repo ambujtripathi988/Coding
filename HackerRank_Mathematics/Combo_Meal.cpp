e2f95a4eac1026b20dd2c27c22b4f1ef72e27b93
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-04-12 17:58:09 +0530

    Create Combo_Meal.cpp

---
+++ HackerRank_Mathematics/Combo_Meal.cpp
@@ -0,0 +1,21 @@
+
+// https://www.hackerrank.com/challenges/combo-meal/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test , b , s , c ;
+
+    cin >> test ;
+
+    for (int i = 0; i < test ; i++)
+    {
+        cin >> b >> s >> c ;
+
+        cout << (b+s)-c << endl ;       // solve three linear equation
+    }
+
+    return 0;
+}
