995727c2227e7227e560887c03683213dd07b17b
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-04-13 18:54:05 +0530

    Create Number_Groups.cpp

---
+++ HackerRank_Mathematics/Number_Groups.cpp
@@ -0,0 +1,18 @@
+
+// https://www.hackerrank.com/challenges/number-groups/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main() 
+{
+    long long int k ;
+    
+    cin >> k ;
+
+    long long int x =  (k*k)*k ;
+    
+    cout << x ;
+    
+    return 0;
+}
