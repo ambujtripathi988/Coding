9e6d4b34cbc685a8efefbb537b0800aae91eedbe
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-04-17 10:40:18 +0530

    Create Sales_by_Match.cpp

---
+++ HackerRank_Problem_Solving/Sales_by_Match.cpp
@@ -0,0 +1,42 @@
+
+// https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=profile
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n , ind , sum = 0 ;
+
+    cin >> n ;
+
+    int socks[n] ;
+
+    int socks_color[100] = {0} ;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> socks[i] ;
+    }
+
+    for (int i = 0; i < n ; i++)
+    {
+        ind = socks[i] ;
+
+        socks_color[ind-1] ++ ;
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        socks_color[i] /= 2 ;
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        sum += socks_color[i] ;
+    }
+
+    cout << sum ;
+
+    return 0;
+}
