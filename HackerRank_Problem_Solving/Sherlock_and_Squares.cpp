4f8bcc41f839efa056b6e5df9b4bb253b13e6428
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-06 18:19:37 +0530

    Create Sherlock_and_Squares.cpp

---
+++ HackerRank_Problem_Solving/Sherlock_and_Squares.cpp
@@ -0,0 +1,38 @@
+
+// https://www.hackerrank.com/challenges/sherlock-and-squares/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int a , b , count = 0 , test ; 
+    
+    cin >> test ;
+
+    for(int k = 0 ; k < test ; k++)
+    {
+        cin >> a >> b ;
+
+        count = 0 ;
+
+        int x = 1 ;
+
+        while( (x*x) < a)
+        {
+            x++ ;
+        }
+
+        while( (x*x) <= b)
+        {
+            count ++ ;
+
+            x++ ;
+        }
+
+        cout << count << endl ;
+
+    }
+
+    return 0;
+}
