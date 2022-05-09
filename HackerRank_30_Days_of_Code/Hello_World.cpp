b1bf8a991095bd1a6d073104cbedb077aaf0bf32
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-09 21:00:16 +0530

    Create Hello_World.cpp

---
+++ HackerRank_30_Days_of_Code/Hello_World.cpp
@@ -0,0 +1,20 @@
+
+// https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=false
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    string str ;
+
+    getline(cin , str) ;
+
+    cout << "Hello, World." << endl << str ;
+    
+    return 0;
+}
