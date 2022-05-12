b0de1e5551419513fa38677448d6f69ace390678
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-12 19:58:51 +0530

    Create Operators.cpp
    Various operators in C++

---
+++ HackerRank_30_Days_of_Code/Operators.cpp
@@ -0,0 +1,22 @@
+
+// https://www.hackerrank.com/challenges/30-operators/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    double meal , tip_price , tip_percent , tax_price , tax_percent ;
+
+    cin >> meal >> tip_percent >> tax_percent ;
+
+    tip_price = ( tip_percent / 100 ) * meal ;
+
+    tax_price = ( tax_percent / 100 ) * meal ;
+
+    meal = meal + tip_price + tax_price ;
+
+    cout << round(meal) << endl ;
+    
+    return 0;
+}
