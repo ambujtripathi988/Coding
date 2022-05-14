8fbf79e8aa02796577cfecbfa4bf69c8e38e182e
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-14 08:57:05 +0530

    Create Loops.cpp

---
+++ HackerRank_30_Days_of_Code/Loops.cpp
@@ -0,0 +1,19 @@
+
+// https://www.hackerrank.com/challenges/30-loops/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n ; 
+
+    cin >> n ;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << n << " x " << (i+1) << " = " << n*(i+1) << endl ;
+    }
+
+    return 0;
+}
