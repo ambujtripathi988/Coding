af6f9cc60f6423b53b658d7c8caa33a9999418b4
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-15 19:31:00 +0530

    Create Let's_Review.cpp

---
+++ HackerRank_30_Days_of_Code/Let's_Review.cpp
@@ -0,0 +1,38 @@
+
+// https://www.hackerrank.com/challenges/30-review-loop/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n ; 
+
+    cin >> n ;
+
+    string str , even , odd ;
+
+    for (int j = 0; j < n ; j++)
+    {
+        string str , even , odd ;
+
+        cin >> str ;
+
+        for(int i=0; i < str.size(); i++)
+        { 
+            if( (i%2) == 0 )
+            {
+                even += str[i] ;
+            }
+
+            else
+            {
+                odd += str[i] ;
+            }
+        }
+        
+        cout << even << " " << odd << endl ;
+    }
+
+    return 0;
+}
