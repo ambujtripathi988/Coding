e73fa5f4b126f268964fe6ca1c3ecc9e45a41ae1
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-18 19:30:07 +0530

    Create Recursion_3.cpp

---
+++ HackerRank_30_Days_of_Code/Recursion_3.cpp
@@ -0,0 +1,35 @@
+
+// https://www.hackerrank.com/challenges/30-recursion/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) 
+{
+    if(n < 0)
+    {    
+        return(-1); /*Wrong value*/      
+    }
+    
+    if(n == 0)
+    {   
+        return(1);  /*Terminating condition*/    
+    }
+    
+    else    
+    {    
+        return(n*factorial(n-1));        
+    }  
+
+}
+
+int main()
+{
+    int n ;
+    
+    cin >> n ;
+    
+    cout << factorial(n) ;
+
+    return 0 ;
+}
