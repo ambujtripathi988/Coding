95f29edd9bbbbe856319ee4490cacb633c1cd559
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-25 18:12:18 +0530

    Create Nested_Logic.cpp

---
+++ HackerRank_30_Days_of_Code/Nested_Logic.cpp
@@ -0,0 +1,54 @@
+
+// https://www.hackerrank.com/challenges/30-nested-logic/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int d1 , m1 , y1 , d2 , m2 , y2 , fine = 0 ;
+
+    cin >> d1 >> m1 >> y1 >> d2 >> m2 >> y2 ;
+
+    if (y1 < y2)
+    {
+        cout << "0" ;
+    }
+
+    else if (y1 == y2 )
+    {
+        if (m1 < m2)
+        {
+            cout << "0" ;
+        }
+        
+        else if (m1 == m2)
+        {
+            if(d1 <= d2)
+            {
+                cout << "0" ;
+            }
+
+            else
+            {
+                fine = ( abs(d1-d2)*15 ) ;
+
+                cout << fine ;
+            }
+        }
+
+        else
+        {
+            fine = ( abs(m1-m2)*500 ) ;
+
+            cout << fine ;
+        }
+    }
+
+    else
+    {
+        cout << "10000" ;
+    }
+
+    return 0;
+}
