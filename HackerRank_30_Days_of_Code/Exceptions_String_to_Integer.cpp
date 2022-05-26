79192eef7033af6c7e2a2776312e116083136f3c
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-05-26 18:18:26 +0530

    Create Exceptions_String_to_Integer.cpp

---
+++ HackerRank_30_Days_of_Code/Exceptions_String_to_Integer.cpp
@@ -0,0 +1,23 @@
+
+// https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string s;
+    cin >> s;
+    
+    try 
+    {
+        cout << stoi(s) << endl;
+    } 
+    
+    catch(...) 
+    {
+        cout << "Bad String" << endl;
+    }
+    
+    return 0;
+}
