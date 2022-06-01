8d8d324f6d7c250cece4885d938a42055d78e74c
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-06-01 22:15:23 +0530

    Create Bigger_is_Greater.cpp

---
+++ HackerRank_Problem_Solving/Bigger_is_Greater.cpp
@@ -0,0 +1,31 @@
+
+// https://www.hackerrank.com/challenges/bigger-is-greater/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test ;
+
+    cin >> test ;
+
+    string str ;
+
+    while(test--)
+    {
+        cin >> str ;
+
+        if( next_permutation(str.begin(), str.end()) )      // next_permutation() : https://www.geeksforgeeks.org/stdnext_permutation-prev_permutation-c/    
+        {
+            cout << str.c_str() << endl ;                   // c_str() : https://www.geeksforgeeks.org/basic_string-c_str-function-in-c-stl/
+        }
+
+        else
+        {
+            cout << "no answer" << endl ;
+        }
+    }
+
+    return 0;
+}
