14805dfc48c49e7e38cd9644f710a6826e11d937
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-06-07 15:31:20 +0530

    Create Left_Rotation.cpp

---
+++ HackerRank_Data_Structure/Left_Rotation.cpp
@@ -0,0 +1,39 @@
+
+// https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n , temp , rotations ;
+
+    cin >> n >> rotations ;
+
+    int arr[n] ;
+
+    for (int i = 0; i < n; i++ )
+    {
+        cin >> arr[i] ;
+    }
+    
+    for (int i = 0; i < rotations; i++)              // rotation
+    {
+        temp = arr[0] ;
+        
+        for (int j = 0 ; j <= (n-1)  ; j++)
+        {            
+            arr[j] = arr[j+1] ;
+        }
+
+        arr[n-1] = temp ;
+
+    }
+
+    for (int i = 0; i < n; i++ )
+    {
+        cout << arr[i] << " " ;
+    }
+
+    return 0;
+}
