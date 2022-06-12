d18ed71300ef02d728be87b0c203ca1c02fed1d4
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-06-12 09:51:09 +0530

    Create CamelCase.cpp

---
+++ HackerRank_Problem_Solving/CamelCase.cpp
@@ -0,0 +1,26 @@
+
+// https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=false
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string str ;
+
+    int count = 1 ;
+
+    cin >> str ;
+
+    for(int i = 0 ; i < str.size() ; i++)
+    {
+        if(isupper(str[i]))
+        {
+            count ++ ;
+        }
+    }
+
+    cout << count << endl ;
+    
+    return 0;
+}
