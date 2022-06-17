cedd6293e84e1bdbee7fa5007947b494981bb403
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-06-17 19:30:01 +0530

    Create Beautiful_Binary_String_Method_1.cpp

---
+++ HackerRank_Problem_Solving/Beautiful_Binary_String_Method_1.cpp
@@ -0,0 +1,32 @@
+
+// https://www.hackerrank.com/challenges/beautiful-binary-string/problem?isFullScreen=false
+
+// Method 1 :
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n , count = 0 ;
+
+    cin >> n ;
+
+    string str ;
+
+    cin >> str ;    
+
+    for(int i = 0 ; i < str.length() ; i++)
+    {
+        if(str.substr(i,3) == "010")
+        { 
+            count ++ ;
+            
+            i += 2 ;
+        }
+    }
+
+    cout << count << endl ;
+    
+    return 0 ;
+}
