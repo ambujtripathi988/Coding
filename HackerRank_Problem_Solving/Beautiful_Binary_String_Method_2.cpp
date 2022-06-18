f9e9e906bbd39a3f001fb538e11aafadb83a3773
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-06-18 09:28:40 +0530

    Create Beautiful_Binary_String_Method_2.cpp

---
+++ HackerRank_Problem_Solving/Beautiful_Binary_String_Method_2.cpp
@@ -0,0 +1,51 @@
+
+// https://www.hackerrank.com/challenges/beautiful-binary-string/problem?isFullScreen=false
+
+// Method 2 :
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n , count = 0 ;
+
+    cin >> n ;
+
+    string str ;
+
+    cin >> str ;    
+
+    for(int i = 2 ; i < n ; i++)
+    {
+        // Both if statements are equivalent
+
+        /*
+
+        if(str[i] == '0' && str[i-1] == '1' && str[i-2] == '0')
+        {
+            count ++ ;
+            
+            i += 2 ;                     
+        }
+
+        */      
+
+        if(str[i] == '0')
+        {
+            if(str[i-1] == '1')
+            {
+                if(str[i-2] == '0')
+                {
+                    count ++ ;
+                    
+                    i += 2 ;
+                }
+            }
+        }
+    }
+
+    cout << count << endl ;
+    
+    return 0 ;
+}
