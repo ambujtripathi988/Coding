2e6d213560b0b22681825f8da4d1e9df11bd6091
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-02 16:05:58 +0530

    Create Arithmetic_Operators.py

---
+++ HackerRank_Python/Arithmetic_Operators.py
@@ -0,0 +1,11 @@
+
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=false
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
