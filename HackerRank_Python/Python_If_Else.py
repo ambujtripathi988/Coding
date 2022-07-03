88593796b8d2bfda97d2ed6cadcc548432aed26b
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-03 10:19:27 +0530

    Create Python_If_Else.py

---
+++ HackerRank_Python/Python_If_Else.py
@@ -0,0 +1,27 @@
+
+# https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=false
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+    if(n%2 == 1):
+        print("Weird")
+        
+    elif( n%2 == 0 ):
+        if((n >= 2 and n <= 6)):
+            print("Not Weird")
+        
+        elif((n >= 6 and n <= 20)):
+            print("Weird")
+            
+        elif(n > 20 ):
+            print("Not Weird")
+        
