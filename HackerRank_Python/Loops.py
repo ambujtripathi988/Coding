cfb2d631dc40915cad5a0f94a40ae456f097d42c
Ambuj Tripathi <92564160+ambujtripathi988@users.noreply.github.com>
2022-07-05 16:23:41 +0530

    Create Loops.py

--- HackerRank_Python/Loops.py
+++ HackerRank_Python/Loops.py
@@ -2,7 +2,7 @@
 # https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=false
 
 if __name__ == '__main__':
-    n = int(raw_input())
+    n = int(raw_input()) 
     
     for i in range(n):
         print(i*i) 
