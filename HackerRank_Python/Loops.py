ccd2bfd2e17fc663e2bce243e0d8aad0b5ff4fe9
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-05 16:22:13 +0530

    Create Loops.py

---
+++ HackerRank_Python/Loops.py
@@ -0,0 +1,9 @@
+
+# https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=false
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    
+    for i in range(n):
+        print(i*i)
+
