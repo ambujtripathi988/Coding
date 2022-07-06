5fc256989a3bcfe121b48ebc7964c9e3717b20e5
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-06 21:18:52 +0530

    Create Write_a_function.py

---
+++ HackerRank_Python/Write_a_function.py
@@ -0,0 +1,13 @@
+
+# https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=false
+
+def is_leap(year):
+    leap = False
+    
+    if( (year%4 == 0) and (year % 400 == 0 or year % 100 != 0) ):
+        leap = True
+    
+    return leap
+
+year = int(raw_input())
+print is_leap(year)
