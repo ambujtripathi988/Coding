8125fec2294b6b545a6ebb2ac0575d64f50b76c4
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-07 18:33:12 +0530

    Create Print_Function.py

---
+++ HackerRank_Python/Print_Function.py
@@ -0,0 +1,11 @@
+
+# https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=false
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    for i in range(n):
+        print(i+1 , end = "")
+
