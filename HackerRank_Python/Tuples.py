847e8f971212a18889a9c7f0993e8527ade684fd
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-12 22:23:16 +0530

    Create Tuples.py

---
+++ HackerRank_Python/Tuples.py
@@ -0,0 +1,12 @@
+
+# https://www.hackerrank.com/challenges/python-tuples/problem?isFullScreen=false
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    tup = ()
+    for x in integer_list:
+        tup+=(x,)
+    
+    print(hash(tup))
+
