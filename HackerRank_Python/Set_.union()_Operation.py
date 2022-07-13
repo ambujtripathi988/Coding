4af36ac9661213914f381fd5f804c8d0f9781520
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-13 12:30:52 +0530

    Create Set_.union()_Operation.py

---
+++ HackerRank_Python/Set_.union()_Operation.py
@@ -0,0 +1,12 @@
+
+# https://www.hackerrank.com/challenges/py-set-union/problem
+
+n = int(input())
+l = list(input().split())
+m = int(input())
+k = list(input().split())
+
+s1 = set(l)
+s2 = set(k)
+
+print(len(s1.union(s2)))
