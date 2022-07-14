959a686458619c2903a49c6b73ab04551ea4c8eb
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-14 08:39:02 +0530

    Create sWAP_cASE.py

---
+++ HackerRank_Python/sWAP_cASE.py
@@ -0,0 +1,17 @@
+
+# https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=false
+
+def swap_case(s):
+    res = ''
+    for ch in s:
+        if ch.isupper():
+            res += ch.lower()
+        else:
+            res += ch.upper()
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
