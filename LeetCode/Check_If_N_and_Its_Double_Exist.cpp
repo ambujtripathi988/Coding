7d7a22fa5b32e758a147beb81214b271e65d4886
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-24 00:57:13 +0530

    Create Check_If_N_and_Its_Double_Exist.cpp

---
+++ LeetCode/Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,50 @@
+
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) 
+    {
+        set<int> s ;
+        
+        int flag = 0 , zero_count = 0 ;
+        
+        for(int i = 0 ; i < arr.size() ; i++)
+        {
+            s.insert(arr[i]) ;
+        }
+        
+        for(int i = 0 ; i < arr.size() ; i++)
+        {   
+            if(arr[i] != 0)
+            {
+                if(s.find(arr[i]*2) != s.end())
+                {
+                    flag = 1 ;
+
+                    break ;
+                }    
+            }
+            
+            if(arr[i] == 0)
+            {
+                zero_count ++ ;
+            }
+        }
+        
+        if(zero_count > 1)
+        {
+            flag = 1 ;
+        }
+        
+        if(flag)
+        {
+            return true ;
+        }
+        
+        else
+        {
+            return false ;
+        }
+    }
+};
