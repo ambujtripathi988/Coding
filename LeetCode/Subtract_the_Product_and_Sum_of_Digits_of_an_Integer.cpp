781c61b0f1dca65c92454ab72964f1e5adb93720
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-25 14:26:56 +0530

    Create Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cpp

---
+++ LeetCode/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cpp
@@ -0,0 +1,30 @@
+
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) 
+    {
+        int product = n , sum = n , add = 0 , multi = 1 ;
+        
+        vector<int> pro ;
+        
+        while(product > 0)
+        {
+            int digit = product % 10 ;
+            
+            product /= 10 ;
+                        
+            pro.push_back(digit) ;
+        }
+        
+        for(int i = 0 ; i < pro.size() ; i++)
+        {
+            add += pro[i] ;
+            
+            multi *= pro[i] ;
+        }
+        
+        return (multi - add) ;
+    }
+};
