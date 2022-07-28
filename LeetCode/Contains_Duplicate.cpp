9c1abc58532169f05f666d777cc3e1016d1e86c4
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-28 11:21:57 +0530

    Create Contains_Duplicate.cpp

---
+++ LeetCode/Contains_Duplicate.cpp
@@ -0,0 +1,25 @@
+
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        set<int> repeat ;
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            repeat.insert(nums[i]) ;
+        }
+        
+        if(nums.size() == repeat.size())
+        {
+            return false ;
+        }
+        
+        else
+        {
+            return true ;
+        }
+    }
+};
