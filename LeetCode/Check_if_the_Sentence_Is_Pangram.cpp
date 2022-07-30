d623ac73657978abc985ef19ad2cfa88604aa114
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-07-30 23:32:30 +0530

    Create Check_if_the_Sentence_Is_Pangram.cpp

---
+++ LeetCode/Check_if_the_Sentence_Is_Pangram.cpp
@@ -0,0 +1,32 @@
+
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) 
+    {
+        int arr[26] = {0} ;
+        
+        for(int i = 0 ; i < sentence.length() ; i++)
+        {
+            arr[sentence[i] - 'a'] = 1 ;
+        }
+        
+        int sum = 0 ;
+        
+        for(int i = 0 ; i < 26 ; i++)
+        {
+            sum += arr[i] ;
+        }
+        
+        if(sum == 26)
+        {
+            return true ;
+        }
+        
+        else
+        {
+            return false ;
+        }
+    }
+};
