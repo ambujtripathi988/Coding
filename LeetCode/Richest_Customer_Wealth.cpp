c7b7750fbbb43745e51498f4a09c554f79072b4f
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-08-01 11:21:49 +0530

    Create Richest_Customer_Wealth.cpp

---
+++ LeetCode/Richest_Customer_Wealth.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& account) 
+    {
+        vector<int> money ; 
+        
+        for(int i = 0 ; i < account.size() ; i++)
+        {
+            int sum = 0 ;
+            
+            for(int k = 0 ; k < account[i].size() ; k++)
+            {
+                sum += account[i][k] ;
+            }
+            
+            money.push_back(sum) ;
+        }
+        
+        int maxi = *max_element(money.begin() , money.end()) ;  // complexity : O(n)
+        
+        return maxi ;
+    }
+};
