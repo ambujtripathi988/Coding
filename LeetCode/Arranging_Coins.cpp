82a9fd54e633c4a182d353a3f2938c1e5b8dbead
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-08-07 14:07:22 +0530

    Create Arranging_Coins.cpp

---
+++ LeetCode/Arranging_Coins.cpp
@@ -0,0 +1,29 @@
+
+// https://leetcode.com/problems/arranging-coins/
+
+class Solution {
+public:
+    int arrangeCoins(int n) 
+    {
+        int count = 0 , rows = 1 ;
+        
+        while(true)
+        {
+            if(n >= rows)
+            {
+                count ++ ;
+                
+                n -= rows ;
+                
+                rows ++ ;
+            }
+            
+            else
+            {
+                break ;
+            }
+        }
+        
+        return count ; 
+    }
+};
