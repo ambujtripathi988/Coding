0b9c133c3c33e26a52aa7bae5ff812fc9997dae9
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-08-11 21:18:51 +0530

    Create Shuffle_String.cpp

---
+++ LeetCode/Shuffle_String.cpp
@@ -0,0 +1,17 @@
+
+// https://leetcode.com/problems/shuffle-string/
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) 
+    {
+        string str = s ;
+        
+        for(int i = 0 ; i < indices.size() ; i++)
+        {
+            str[indices[i]] = s[i] ;
+        }
+        
+        return str ;
+    }
+};
