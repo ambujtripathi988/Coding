27b2ab7ace812e29f4984af403ae72a519502f48
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-08-20 22:22:23 +0530

    Create Detect-Capital.cpp

---
+++ LeetCode/Detect-Capital.cpp
@@ -0,0 +1,35 @@
+
+// https://leetcode.com/problems/detect-capital/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) 
+    {
+        int upper = 0 ;
+        
+        for(int i = 0 ; i < word.length() ; i++)
+        {
+            if(isupper(word[i]))
+            {
+                upper ++ ;
+            }
+        }
+        
+        int count = 0 ;
+        
+        if(isupper(word[0]))
+        {
+            count = 1 ;
+        }
+        
+        if(upper == 0 || (upper == 1 && count == 1 )  || upper == word.length())
+        {
+            return true ;
+        }
+        
+        else
+        {
+            return false ;
+        }
+    }
+};
