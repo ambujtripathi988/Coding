caeb7be96813498abf17b98777d54ddf4c06e6a6
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-08-22 08:09:26 +0530

    Create Two_Sum_Method_2.cpp

---
+++ LeetCode/Two_Sum_Method_2.cpp
@@ -0,0 +1,33 @@
+
+// https://leetcode.com/problems/two-sum/
+
+// Method 2 : Time Complexity - O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> ans ;
+        
+        map<int,int> mape ;
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            mape[nums[i]] = i ;
+        }
+        
+        for(int j = 0 ; j < nums.size() ; j++)
+        {
+            if(mape[(target-nums[j])] && mape[target-nums[j]] != j)
+            {
+                ans.push_back(j) ;
+                
+                ans.push_back(mape[(target - nums[j])]) ;
+                
+                break ;
+            }
+        }
+            
+        return ans ;
+    }
+};
