1008caac2b680a469b61a42722de57aad4401fe7
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-08-24 09:24:16 +0530

    Create First_Unique_Character_in_a_String.cpp

---
+++ LeetCode/First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,31 @@
+
+// https://leetcode.com/problems/first-unique-character-in-a-string/
+
+class Solution {
+public:
+    int firstUniqChar(string s) 
+    {
+        map<char,int> repeat ;
+        
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            repeat[s[i]] = repeat[s[i]] + 1 ;
+        }
+        
+        char ch ;
+        
+        int ind = -1 ;
+        
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            if(repeat[s[i]] == 1)
+            {
+                ind = i ;
+                
+                break ;
+            }
+        }
+        
+        return ind ;
+    }
+};
