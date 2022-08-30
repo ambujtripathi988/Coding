bc9b2735f32bd1e94acb546fadf9dfa5eb21d5f4
Ambuj Tripathi <92564160+ambujtripathi988@users.noreply.github.com>
2022-08-30 09:47:03 +0530

    Create Check_If_Two_String_Arrays_are_Equivalent.cpp

--- LeetCode/Check_If_Two_String_Arrays_are_Equivalent.cpp
+++ LeetCode/Check_If_Two_String_Arrays_are_Equivalent.cpp
@@ -1,7 +1,7 @@
 
 // https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
 
-class Solution {
+class Solution { 
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
     {
