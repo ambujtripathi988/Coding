0f2ba4f2d8ae0c604e58bf4f6a6374a735f05f9a
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-03 10:23:41 +0530

    Create Height_Checker.cpp

---
+++ LeetCode/Height_Checker.cpp
@@ -0,0 +1,23 @@
+
+// https://leetcode.com/problems/height-checker/
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) 
+    {
+        vector<int> vect  ;
+        
+        int count = 0 ;
+        
+        for(int i = 0 ; i < heights.size() ; i++)
+            vect.push_back(heights[i]) ;
+        
+        sort(vect.begin() , vect.end()) ;           // time complexity : N log(N)
+        
+        for(int i = 0 ; i < vect.size() ; i++)
+            if(vect[i] != heights[i])
+                count ++ ;
+        
+        return count ;
+    }
+};
