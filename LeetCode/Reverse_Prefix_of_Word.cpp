575a4a7eea749734a060c6abd27077930ac3b04c
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-05 09:28:03 +0530

    Create Reverse_Prefix_of_Word.cpp

---
+++ LeetCode/Reverse_Prefix_of_Word.cpp
@@ -0,0 +1,40 @@
+
+// https://leetcode.com/problems/reverse-prefix-of-word/
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) 
+    {
+        int flag = 0 , inde ;
+        
+        for(int i = 0 ; i < word.length() ; i++)
+        {   
+            if(word[i] == ch)
+            {
+                flag = 1 ;
+                
+                inde = i ;
+                
+                break ;
+            }
+        }
+        
+        if(!flag)
+            return word ;
+        
+        else
+        {
+            string str ;
+            
+            for(int i = 0 ; i <= inde ; i++)
+                str.push_back(word[i]) ;
+            
+            reverse(str.begin() , str.end()) ;
+            
+            for(int i = 0 ; i <= inde ; i++)
+                word[i] = str[i] ;
+            
+            return word ;
+        }
+    }
+};
