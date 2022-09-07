1228cf887b5493087143692b07c6f1cdd4c1d4c7
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-07 18:17:18 +0530

    Create Find_Closest_Number_to_Zero.cpp

---
+++ LeetCode/Find_Closest_Number_to_Zero.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/find-closest-number-to-zero/
+
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) 
+    {
+        int minDist = nums[0] ;
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            int n = abs(nums[i]) ;
+            
+            if(n < abs(minDist))
+                minDist = nums[i] ;
+            
+            else if(n == abs(minDist))
+                minDist = max(nums[i] , minDist) ;
+            
+            else if(n == 0)
+                return 0 ;
+        }    
+        
+        return minDist ;
+    }
+};
