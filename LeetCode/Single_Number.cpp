6f7730409480f851e8f9ceb8dc7c08666e1c7fcf
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-12 18:43:00 +0530

    Create Single_Number.cpp

---
+++ LeetCode/Single_Number.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        map<int,int> rep ;
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+            rep[nums[i]] = rep[nums[i]] + 1 ;
+        
+        int no ;
+        
+        for(auto iter = rep.begin() ; iter!= rep.end() ; iter++)
+        {    
+            if(iter -> second == 1)
+            {
+                no = iter -> first ;
+                break ;
+            }
+        }
+        
+        return no ;
+    }
+};
