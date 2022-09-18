b7fdc49e11488f81a1d7587141fbbf47e69e0507
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-18 21:33:16 +0530

    Create Find_Lucky_Integer_in_an_Array.cpp

---
+++ LeetCode/Find_Lucky_Integer_in_an_Array.cpp
@@ -0,0 +1,30 @@
+
+// https://leetcode.com/problems/find-lucky-integer-in-an-array/
+
+class Solution {
+public:
+    int findLucky(vector<int>& arr) 
+    {
+        map<int,int> rep ;
+
+        for(int i = 0 ; i < arr.size() ; i++)
+            rep[arr[i]] = rep[arr[i]] + 1 ;
+
+        int flag = 0 , num ;
+
+        for(auto iter = rep.begin() ; iter != rep.end() ; iter++)
+        {   if(iter -> first == iter -> second)
+            {
+                num = iter -> first ;
+
+                flag = 1 ;
+            }
+        } 
+
+        if(!flag)
+            return -1 ;
+
+        else
+            return num ;
+    }
+};
