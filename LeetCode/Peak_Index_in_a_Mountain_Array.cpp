c90820989aa65649443c219d1853eb0346d75231
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-20 18:45:20 +0530

    Create Peak_Index_in_a_Mountain_Array.cpp

---
+++ LeetCode/Peak_Index_in_a_Mountain_Array.cpp
@@ -0,0 +1,27 @@
+
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+// Method 2 : O(log N)
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) 
+    {
+        int s = 0 , e = arr.size() - 1 ;
+        
+        int mid = s + (e-s)/2 ;
+        
+        while(s < e)
+        {
+            mid = s + (e-s)/2 ;
+            
+            if(arr[mid] < arr[mid+1])
+                s = mid + 1 ;
+            
+            else
+                e = mid ;
+        }
+        
+        return s ;
+    }
+};
