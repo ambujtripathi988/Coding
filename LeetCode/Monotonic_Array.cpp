2352d9fb099cffbc3d43378f7981e8c28b300db9
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-25 11:03:50 +0530

    Create Monotonic_Array.cpp

---
+++ LeetCode/Monotonic_Array.cpp
@@ -0,0 +1,40 @@
+
+// https://leetcode.com/problems/monotonic-array/
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        int flag1 = 0 , flag2 = 0 ;
+        
+        for(int i = 0 ; i < nums.size() - 1 ; i++)
+        {
+            if(nums[i] <= nums[i+1])
+                continue ;
+            
+            else
+            {
+                flag1 = 1 ;
+                break ;
+            }
+        }
+        
+        for(int i = 0 ; i < nums.size() - 1 ; i++)
+        {
+            if(nums[i] >= nums[i+1])
+                continue ;
+            
+            else
+            {
+                flag2 = 1;
+                break ;
+            }
+        }
+        
+        if(flag1 == 1 && flag2 == 1)
+            return false ;
+        
+        else 
+            return true ;
+    }
+};
