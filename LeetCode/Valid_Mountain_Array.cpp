3d069270fc76f4b272074a3048d544bf7108d0bd
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-09-27 00:36:43 +0530

    Create Valid_Mountain_Array.cpp

---
+++ LeetCode/Valid_Mountain_Array.cpp
@@ -0,0 +1,73 @@
+
+// https://leetcode.com/problems/valid-mountain-array/
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) 
+    {
+        int flag = 0 , ind ;
+        
+        if(arr.size() == 1)
+            return false ;
+        
+        if(arr[0] > arr[1])
+            return false ;
+        
+        if(arr[arr.size() - 1] >= arr[arr.size() - 2])
+            return false ;
+        
+        for(int i = 0 ; i < arr.size() - 1 ; i++)
+        {
+            if(arr[i] == arr[i+1])
+            {
+                flag = 1 ;
+                break ;
+            }
+        }
+        
+        if(flag)
+            return false ;
+        
+        for(int i = 0 ; i < arr.size() - 1 ; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                ind = i ;
+                break ;
+            }
+        }
+        
+        for(int i = 0 ; i < ind - 1 ; i++)
+        {
+            if(arr[i] < arr[i+1])
+                continue ;
+            
+            else
+            {
+                flag = 1 ;
+                break ;
+            }
+        }
+        
+        if(flag)
+            return false ;
+        
+        for(int i = ind ; i < arr.size() - 1 ; i++)
+        {
+            if(arr[i] > arr[i+1])
+                continue ;
+            
+            else
+            {
+                flag = 1 ;
+                break ;
+            }
+        }
+        
+        if(flag)
+            return false ;
+        
+        else
+            return true ;
+    }
+};
