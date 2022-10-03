95e5dcaa8053f5fe7e1ca6a406824150b952b32e
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-03 19:21:47 +0530

    Create Jewels_and_Stones.cpp

---
+++ LeetCode/Jewels_and_Stones.cpp
@@ -0,0 +1,21 @@
+
+// https://leetcode.com/problems/jewels-and-stones/
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) 
+    {
+        set<char> sona ;
+        
+        for(int i = 0 ; i < jewels.length() ; i++)
+            sona.insert(jewels[i]) ;
+        
+        int count = 0 ; 
+        
+        for(int i = 0 ; i < stones.length() ; i++)
+            if(sona.find(stones[i]) != sona.end())
+                count ++ ;
+        
+        return count ;
+    }
+};
