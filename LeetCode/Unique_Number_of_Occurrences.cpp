bef116748f720b705f5f01711166fe239d6d54ee
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-07 22:02:01 +0530

    Create Unique_Number_of_Occurrences.cpp

---
+++ LeetCode/Unique_Number_of_Occurrences.cpp
@@ -0,0 +1,27 @@
+
+// https://leetcode.com/problems/unique-number-of-occurrences/
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) 
+    {
+        map<int,int> rep ;\
+        set<int> a ;
+        set<int> len ;
+        
+        for(int i = 0 ; i < arr.size() ; i++)
+        {    
+            rep[arr[i]] = rep[arr[i]] + 1 ;
+            a.insert(arr[i]) ;
+        }
+        
+        for(auto iter = rep.begin() ; iter != rep.end() ; iter++)
+            len.insert(iter -> second) ;
+        
+        if(len.size() == a.size())
+            return true ;
+        
+        else
+            return false ;
+    }
+};
