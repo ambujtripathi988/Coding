c97542468f48dc73b28d862d6058f11e2a98d9ac
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-09 01:34:13 +0530

    Create Rings_and_Rods.cpp

---
+++ LeetCode/Rings_and_Rods.cpp
@@ -0,0 +1,46 @@
+
+// https://leetcode.com/problems/rings-and-rods/
+
+class Solution {
+public:
+    int countPoints(string rings) 
+    {
+        vector<string> rod ;
+        
+        for(int i = 0 ; i < 10 ; i++)
+            rod.push_back("") ;
+        
+        for(int i = 0 ; i < rings.length() - 1 ; i += 2)
+        {
+            int x = rings[i+1] - '0' ;
+            
+            rod[x].push_back(rings[i]) ;
+        }
+        
+        int count = 0 ;
+        
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            string str = rod[i] ;
+            
+            int r = 0 , g = 0 , b = 0 ;
+            
+            for(int j = 0 ; j < str.length() ; j++)
+            {
+                if(str[j] == 'R')
+                    r = 1 ;
+                
+                else if(str[j] == 'G')
+                    g = 1 ;
+                
+                else 
+                    b = 1 ;
+            }
+            
+            if(r == 1 && g == 1 && b ==1)
+                count ++ ;
+        }
+        
+        return count ;
+    }
+};
