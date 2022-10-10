ff83fc73a71aee2a18d10108b0d06f662c052f71
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-11 01:04:16 +0530

    Create Kth_Distinct_String_in_an_Array.cpp

---
+++ LeetCode/Kth_Distinct_String_in_an_Array.cpp
@@ -0,0 +1,32 @@
+
+// https://leetcode.com/problems/kth-distinct-string-in-an-array/
+
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) 
+    {
+        map<string,int> rep ;
+        
+        vector<string> str ;
+        
+        for(int i = 0 ; i < arr.size() ; i++)
+            rep[arr[i]] = rep[arr[i]] + 1 ;
+        
+        bool flag = false ;
+        
+        for(int i = 0 ; i < arr.size() ; i++)           // map sorts automatically iter -> first
+        {    
+            if(rep[arr[i]] == 1)
+            {
+                str.push_back(arr[i]) ;
+                flag = true ;
+            }
+        }
+        
+        if(!flag)
+            return "" ;
+        
+        else
+            return (str[k - 1]) ;
+    }
+};
