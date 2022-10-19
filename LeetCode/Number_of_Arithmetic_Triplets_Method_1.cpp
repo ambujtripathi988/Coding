34d9c829b10524678fab23ced0b8733095207a7a
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-20 00:32:38 +0530

    Create Number_of_Arithmetic_Triplets_Method_1.cpp

---
+++ LeetCode/Number_of_Arithmetic_Triplets_Method_1.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/number-of-arithmetic-triplets/
+ 
+// Method 1 - Time Complexity : O(n^3)      (Brute Force)
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) 
+    {
+        int count = 0 ; 
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            for(int j = i+1 ; j < nums.size() ; j++)
+            {
+                for(int k = j+1 ; k < nums.size() ; k++)
+                {
+                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
+                        count ++ ;
+                }
+            }
+        }
+        
+        return count ;
+    }
+};
