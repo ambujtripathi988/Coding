5f0ce6f989f5f06a1b80a0c0b04ed2907be61b03
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-23 19:43:56 +0530

    Create Check if Number Has Equal Digit Count and Digit Value.cpp

---
+++ LeetCode/Check if Number Has Equal Digit Count and Digit Value.cpp
@@ -0,0 +1,33 @@
+
+// https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/
+
+class Solution {
+public:
+    bool digitCount(string num) 
+    {
+        map<int,int> rep ;
+        
+        for(int i = 0 ; i < num.length() ; i++)
+        {
+            int x = int(num[i]) - 48 ;              // ascii values
+            rep[x] = rep[x] + 1 ;
+        }
+        
+        bool flag = false ;
+        
+        for(int i = 0 ; i < num.length() ; i++)
+        {
+            if(rep[i] != num[i] - '0')
+            {
+                flag = true ;
+                break ;
+            }
+        }
+        
+        if(flag)
+            return false ;
+        
+        else 
+            return true ;
+    }
+};
