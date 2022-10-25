f5d90f71c23cf7273d8a069b248b2f01f7cad6ec
Ambuj Tripathi <acc.ambujtripathi@gmail.com>
2022-10-25 20:40:25 +0530

    Create Average Salary Excluding the Minimum and Maximum Salary.cpp

---
+++ LeetCode/Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,22 @@
+
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+
+class Solution {
+public:
+    double average(vector<int>& salary) 
+    {
+        int mini = *min_element(salary.begin() , salary.end()) ;
+        int maxi = *max_element(salary.begin() , salary.end()) ;
+        
+        int sum = 0 ;
+        double avg = 0;
+        
+        for(int i = 0 ; i < salary.size() ; i++)
+            if(salary[i] != mini && salary[i] != maxi)
+                sum += salary[i] ;
+        
+        avg =  (sum * 1.0)  / (salary.size() - 2) ;
+        
+        return avg ; 
+    }
+};
