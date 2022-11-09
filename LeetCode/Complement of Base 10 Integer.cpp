6bfa069f118e7559d4515c9ae2fc264b4998c78b
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-11-09 19:49:04 +0530

    Create Complement of Base 10 Integer.cpp

---
+++ LeetCode/Complement of Base 10 Integer.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/complement-of-base-10-integer/
+
+class Solution {
+public:
+    int bitwiseComplement(int n) 
+    {
+        if(!n)
+            return 1 ;          // edge case
+        
+        int m = n ;
+        
+        int num = 0 ;
+        
+        while(m != 0)                   // help from love babbar
+        {
+            num = (num << 1) | 1 ;
+            
+            m = m >> 1 ;
+        }
+        
+        int ans = (~n) & num ;
+        
+        return ans ;
+    }
+};
