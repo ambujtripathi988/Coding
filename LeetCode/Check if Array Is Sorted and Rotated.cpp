29cbab947833f6ca8cc2e4f416d22cccc2edc74f
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-11-17 21:51:21 +0530

    Create Check if Array Is Sorted and Rotated.cpp

---
+++ LeetCode/Check if Array Is Sorted and Rotated.cpp
@@ -0,0 +1,23 @@
+
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) 
+    {
+        int count = 0 ;
+        
+        for(int i = 1 ; i < nums.size() ; i++)
+           if(nums[i-1] > nums[i])
+               count ++ ;
+        
+        if(nums[nums.size() -1] > nums[0])
+            count ++ ;
+        
+        if(count > 1)
+            return false ;
+        
+        else
+            return true ;
+    }
+};
