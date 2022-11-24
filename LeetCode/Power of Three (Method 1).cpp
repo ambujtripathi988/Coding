b853d2f1b486f3c364f3718929616acde15b37f5
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-11-24 19:04:12 +0530

    Create Power of Three (Method 1).cpp

---
+++ LeetCode/Power of Three (Method 1).cpp
@@ -0,0 +1,16 @@
+
+// https://leetcode.com/problems/power-of-three/
+
+// Method 1  
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) 
+    {   
+        for(int i = 0 ; i <= sqrt(n) ; i++)
+            if(n == pow(3,i))
+                return true ;
+        
+        return false ;
+    }
+};
