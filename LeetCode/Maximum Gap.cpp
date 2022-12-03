e478584bd0dadce0162dd5b46723f89a33d9c2ce
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-12-03 11:14:25 +0530

    Create Maximum Gap.cpp

---
+++ LeetCode/Maximum Gap.cpp
@@ -0,0 +1,20 @@
+
+// https://leetcode.com/problems/maximum-gap/
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) 
+    {
+        if(nums.size() < 2)
+            return 0 ;
+        
+        sort(nums.begin() , nums.end()) ;           // set takes more time for soritng
+        
+        int diff = -1 ;
+        
+        for(int i = 1 ; i < nums.size() ; i++)
+            diff = max( diff , abs(nums[i] - nums[i-1]) ) ;
+        
+        return diff ;
+    }
+};
