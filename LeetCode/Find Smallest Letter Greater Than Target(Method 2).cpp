0d9e9cdbb8788acdec2334dd227ff8b9268821cb
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-12-05 09:27:56 +0530

    Create Find Smallest Letter Greater Than Target(Method 2).cpp

---
+++ LeetCode/Find Smallest Letter Greater Than Target(Method 2).cpp
@@ -0,0 +1,25 @@
+
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+// Method 2 : Binary Search O(log(N))
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) 
+    {
+        int start = 0 , end = letters.size() ;
+        
+        while(start < end)
+        {
+            int mid = start + (end-start)/2 ;
+            
+            if(target >= letters[mid])
+                start = mid + 1 ;
+            
+            else
+                end = mid ;
+        }
+        
+        return letters[start % letters.size()] ;
+    }
+};
