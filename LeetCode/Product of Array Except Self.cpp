4970a36ce002cfc487081e9af6baac2c8f709ee0
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2022-12-09 00:38:49 +0530

    Create Product of Array Except Self.cpp

---
+++ LeetCode/Product of Array Except Self.cpp
@@ -0,0 +1,28 @@
+
+// https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        int pre = 1 , post = 1 ;
+        
+        int sizee = nums.size() - 1 ;
+        
+        vector<int> ans(nums.size(),0) ;
+        
+        for(int i = 0 ; i <= sizee ; i++)
+        {
+            ans[i] = pre ;
+            pre *= nums[i] ;
+        }
+        
+        for(int i = sizee ; i>= 0 ; i--) 
+        {
+            ans[i] *= post ;
+            post *= nums[i] ;
+        }
+        
+        return ans ;
+    }
+};
