736a70d740c8beb821eabc6de9066d23f53004e8
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-01-09 09:47:56 +0530

    Create N-Queens II.cpp

---
+++ LeetCode/N-Queens II.cpp
@@ -0,0 +1,87 @@
+
+// https://leetcode.com/problems/n-queens-ii/
+
+class Solution {
+public:
+    
+    bool isSafe(int row ,int col , vector<string> board , int n)
+    {
+        int duprow = row ;
+        int dupcol = col ;
+
+        while(row >= 0 && col >= 0)
+        {
+            if(board[row][col] == 'Q')
+                return false ;
+
+            row -- ;
+            col -- ;
+        } 
+
+        col = dupcol ;
+        row = duprow ;
+
+        while(col >= 0)
+        {
+            if(board[row][col] == 'Q')
+                return false ;
+
+            col -- ;
+        }
+
+        col = dupcol ;
+        row = duprow ;
+
+        while(row < n && col >= 0)
+        {
+            if(board[row][col] == 'Q')
+                return false ;
+
+            row ++ ;
+            col -- ;
+        }
+
+        return true ;
+    }
+
+    void solve(int col , vector<string>& board , vector<vector<string>>& ans, int n , int & count)
+    {
+        // base case 
+
+        if(col == n)
+        {
+            ans.push_back(board) ;
+            count ++ ;
+            return ;
+        }
+
+        for(int row = 0 ; row < n ; row++)
+        {
+            if(isSafe(row,col,board,n))
+            {
+                board[row][col] = 'Q' ;
+
+                solve(col+1 , board , ans , n , count) ;
+
+                board[row][col] = '.' ;
+            }
+        }
+    }
+    
+    int totalNQueens(int n) 
+    {
+        vector<vector<string>> ans ;
+        vector<string> board(n) ;
+
+        string s(n,'.') ;
+
+        for(int i = 0 ; i < n ; i++)
+            board[i] = s ;
+
+        int count = 0 ;
+        
+        solve(0,board,ans,n,count) ;
+    
+        return count ; 
+    }
+};
