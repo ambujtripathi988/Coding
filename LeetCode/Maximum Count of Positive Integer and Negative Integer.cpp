7cce3a532d2b199297c3627c5e749805cbff7d8d
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-03-14 09:28:03 +0530

    Create Maximum Count of Positive Integer and Negative Integer.cpp

---
+++ LeetCode/Maximum Count of Positive Integer and Negative Integer.cpp
@@ -0,0 +1,21 @@
+
+// https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/description/
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) 
+    {
+        int pos = 0 , neg = 0 ;
+
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            if(nums[i] > 0)
+                pos ++ ;
+
+            else if(nums[i] < 0)
+                neg ++ ;
+        }    
+
+        return max(pos,neg) ;
+    }
+};
