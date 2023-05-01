f790ee0426c4f328478201d029baf65aa0f6f2f6
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-05-01 19:57:43 +0530

    Create Jump Game.cpp

---
+++ LeetCode/Jump Game.cpp
@@ -0,0 +1,20 @@
+
+// https://leetcode.com/problems/jump-game/description/
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int reachable = 0 ;
+
+        for(int i = 0 ; i < nums.size() ; i++)   
+        {
+            if(reachable < i)
+                return false ;
+
+            reachable = max(reachable , i + nums[i]) ;
+        }    
+
+        return true ;
+    }
+};
