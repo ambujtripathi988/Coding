50a33e219156a3425d5b9b93a325195e026eb469
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-05-11 09:52:11 +0530

    Create Count Special Quadruplets.cpp

---
+++ LeetCode/Count Special Quadruplets.cpp
@@ -0,0 +1,19 @@
+
+// https://leetcode.com/problems/count-special-quadruplets/
+
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) 
+    {
+        int n = nums.size() , count = 0 ;
+
+        for(int a = 0 ;  a < n ; a++)
+            for(int b = a+1 ; b < n ; b++)
+                for(int c = b +1 ; c < n ;c++)
+                    for(int d = c+1 ; d < n ; d++)
+                        if(a < b && b < c  && c < d && nums[a] + nums[b] + nums[c] == nums[d])
+                            count ++ ;
+
+        return count ;    
+    }
+};
