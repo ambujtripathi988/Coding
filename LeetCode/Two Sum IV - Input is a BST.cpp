50e85a5558937ce936f21c69fc3aacfe4e0bc9a0
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-06-09 22:56:12 +0530

    Create Two Sum IV - Input is a BST.cpp

---
+++ LeetCode/Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,40 @@
+
+// https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool target(TreeNode* root, int k, unordered_set<int> &st){
+        if(!root){
+            return false;
+        }
+        
+        if(target(root->left, k, st)){
+            return true;
+        }
+        int val = root->val;
+        if(st.find(k-val) != st.end()){
+            return true;
+        }
+        st.insert(val);
+        if(target(root->right, k, st)){
+            return true;
+        }
+        return false;
+    }
+    
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> st;
+        return target(root, k, st);
+    }
+};
