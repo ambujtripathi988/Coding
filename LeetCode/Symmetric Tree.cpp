2112271df6655923d5dcb779297f4c74b01168ec
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-06-10 09:11:33 +0530

    Create Symmetric Tree.cpp

---
+++ LeetCode/Symmetric Tree.cpp
@@ -0,0 +1,15 @@
+
+// https://leetcode.com/problems/symmetric-tree/description
+
+class Solution {
+private:
+    bool isEquivalent(TreeNode* left, TreeNode* right) {
+        if (!left || !right) return left == right;
+        else return left->val == right->val && isEquivalent(left->left, right->right) && isEquivalent(right->left, left->right);
+    }
+
+public:
+    bool isSymmetric(TreeNode* root) {
+        return isEquivalent(root->left, root->right);
+    }
+};
