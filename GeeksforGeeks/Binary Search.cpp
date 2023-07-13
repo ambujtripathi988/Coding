155f3e8c46eb6578c7d1a904e4c5f61e9d20cb34
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-07-13 21:52:15 +0530

    Create Binary Search.cpp

---
+++ GeeksforGeeks/Binary Search.cpp
@@ -0,0 +1,56 @@
+
+// https://practice.geeksforgeeks.org/problems/binary-search-1587115620/1?page=2&category[]=Arrays&sortBy=difficulty
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) 
+    {
+        int start = 0 , end = n-1 ;
+        
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2 ;
+            
+            if(k == arr[mid])
+                return mid ;
+                
+            else if(arr[mid] > k)
+                end = mid - 1 ;
+            
+            else
+                start = mid + 1 ; 
+        }
+        
+        return -1 ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
