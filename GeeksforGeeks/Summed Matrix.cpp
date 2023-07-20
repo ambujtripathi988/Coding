c30b37f3c9c09e6124ccde10596f614ec4f32bff
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-07-20 09:31:23 +0530

    Create Summed Matrix.cpp

---
+++ GeeksforGeeks/Summed Matrix.cpp
@@ -0,0 +1,40 @@
+
+// https://practice.geeksforgeeks.org/problems/summed-matrix5834/1?page=1&category[]=Mathematical&category[]=Strings&curated[]=8&sortBy=submissions
+
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) 
+    {
+        if(q>n*2)
+            return 0 ;
+
+        if(q<=n+1)
+            return (q-1);
+
+        else
+            return ((n*2)-q+1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
