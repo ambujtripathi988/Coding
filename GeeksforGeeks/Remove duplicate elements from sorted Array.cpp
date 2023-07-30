98b829f2c97a6822791591710dd58c10c9d74cd3
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-07-30 23:03:43 +0530

    Create Remove duplicate elements from sorted Array.cpp

---
+++ GeeksforGeeks/Remove duplicate elements from sorted Array.cpp
@@ -0,0 +1,59 @@
+
+// https://practice.geeksforgeeks.org/problems/remove-duplicate-elements-from-sorted-array/0
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n)
+    {
+        // code here
+        
+        set<int> s ;
+        
+        for(int i = 0 ; i < n ; i++)
+            s.insert(a[i]) ;
+        
+        int inde = 0 ;
+        
+        for(auto iter = s.begin() ; iter != s.end() ; iter++)
+        {
+            a[inde] = *iter ;
+            inde++ ;
+        }
+        
+        return s.size() ;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}
+// } Driver Code Ends 
