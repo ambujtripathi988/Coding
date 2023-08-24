816513b13c80ae38655ce0f6bd662afd35565617
ambujtripathi988 <acc.ambujtripathi@gmail.com>
2023-08-24 10:44:09 +0530

    Create Lowest Common Ancestor in a Binary Tree.cpp

---
+++ GeeksforGeeks/Lowest Common Ancestor in a Binary Tree.cpp
@@ -0,0 +1,48 @@
+
+// https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1
+
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       //Your code here
+       
+        if(!root)
+            return NULL ;
+            
+        if(root -> data == n1 || root -> data == n2)
+            return root ;
+            
+        Node * leftans = lca(root -> left , n1 , n2) ;
+        Node * rightans = lca(root -> right , n1 , n2) ;
+        
+        if(leftans != NULL && rightans != NULL)
+            return root ;
+            
+        else if(leftans != NULL && rightans == NULL)
+            return leftans ;
+            
+        else if(leftans == NULL && rightans != NULL)
+            return rightans ;
+            
+        else 
+            return NULL ;
+    }
+};
